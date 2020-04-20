b48ac2b7eef3a689e7ccf19b5e02727808b4f4b0
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-21 02:36:28 +0530

    File upload middleware

--- server/middlewares/imageUpload.js
+++ server/middlewares/imageUpload.js
@@ -3,10 +3,10 @@ const { v4 } = require('uuid')
 const path = require('path')
 
 const storage = Multer.diskStorage({
-  destination: function(req, file, callback) {
+  destination: function (req, file, callback) {
     callback(null, 'uploads/')
   },
-  filename: function(req, file, callback) {
+  filename: function (req, file, callback) {
     callback(null, v4() + path.extname(file.originalname))
   }
 })
@@ -15,7 +15,7 @@ const limits = {
   fileSize: 1024 * 1024 * 5
 }
 
-const fileFilter = function(req, file, callback) {
+const fileFilter = function (req, file, callback) {
   const allowedTypes = ['image/jpg', 'image/jpeg']
   if (!allowedTypes.includes(file.mimetype)) {
     callback(new Error('Invalid file type'), false)
