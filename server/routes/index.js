8fca9cba01b97f49d43df9133391aec870c86314
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-21 02:37:39 +0530

    Added image and post routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const Router = require('express').Router()
 const PostRouter = require('./post')
+const ImageRouter = require('./image')
 
 Router.use('/post', PostRouter)
+
+Router.use('/image', ImageRouter)
 
 module.exports = Router
