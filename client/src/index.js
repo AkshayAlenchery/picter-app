b4047f1c8e824c2ab9c2a71ba67abdc0271a8777
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-27 17:19:39 +0530

    Included notification component

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/css/index.css'
 import NewPost from './components/Newpost'
+import Notifications from './components/Notifications'
+import { Provider as NotificationProvider } from './context/NotificationContext'
 
 const App = () => {
   return (
-    <div style={{ width: '100%' }}>
-      <h1>Hello Akshay</h1>
-      <NewPost />
+    <div>
+      <NotificationProvider>
+        <Notifications />
+        <div style={{ width: '100%' }}>
+          <h1>Hello Akshay</h1>
+          <NewPost />
+        </div>
+      </NotificationProvider>
     </div>
   )
 }
