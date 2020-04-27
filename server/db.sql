2b5ae8d28f7c753e18f5a2871955657fb079f5b8
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-27 22:07:49 +0530

    Added users table

--- server/db.sql
+++ server/db.sql
@@ -8,3 +8,19 @@ CREATE TABLE posts
   like_count INTEGER DEFAULT 0,
   comment_count INTEGER DEFAULT 0
 );
+
+CREATE TABLE users
+(
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(100) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email_address VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  gender VARCHAR(6) NOT NULL,
+  city VARCHAR(100) DEFAULT '',
+  bio TEXT DEFAULT '',
+  followers INTEGER DEFAULT 0,
+  following INTEGER DEFAULT 0,
+  registered_on VARCHAR(255) NOT NULL
+);
