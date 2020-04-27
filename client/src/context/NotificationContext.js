7037a28d5f8b849261403f7f05ffa1230e8e58b4
Akshay Alenchery <akshayalenchery@gmail.com>
2020-04-27 17:18:23 +0530

    Completed notification context

---
+++ client/src/context/NotificationContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer } from 'react'
+import { ADD_NOTI, REMOVE_NOTI } from './actionTypes'
+
+// Initial state
+const initialState = []
+
+// Notification reducer
+const reducer = (state, payload) => {
+  switch (payload.action) {
+    case ADD_NOTI:
+      return [payload.data, ...state]
+    case REMOVE_NOTI:
+      return state.filter((noti) => noti.id !== payload.data)
+    default:
+      return state
+  }
+}
+
+// Notification context
+export const Context = createContext()
+
+// Notification Provider
+export const Provider = (props) => {
+  const [notifications, dispatch] = useReducer(reducer, initialState)
+  return <Context.Provider value={{ notifications, dispatch }}>{props.children}</Context.Provider>
+}
