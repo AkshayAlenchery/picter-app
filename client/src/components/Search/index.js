f8a237c3e2d691b78939fde73c2f1bdfb1a1e329
Akshay Alenchery <akshayalenchery@gmail.com>
2020-05-24 21:52:40 +0530

    Refactored code

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Avatar from '../../assets/images/avatar.jpg'
 import './style.css'
 import { Link } from 'react-router-dom'
 
+import { Context as AuthContext } from '../../context/Auth'
+
 export default ({ user }) => {
+  const { authUser } = useContext(AuthContext)
   return (
     <div className='user-row'>
       <div className='user-img'>
@@ -12,7 +15,8 @@ export default ({ user }) => {
       </div>
       <div className='user-det'>
         <Link to={`/${user.username}`}>
-          {user.firstname} {user.lastname}
+          {user.firstname} {user.lastname}{' '}
+          <span style={{ color: '#999' }}>{authUser.user.id === user.id ? '(You)' : ''}</span>
         </Link>
         <p className='bio'>{user.bio}</p>
       </div>
